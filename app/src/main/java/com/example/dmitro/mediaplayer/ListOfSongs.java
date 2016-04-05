94fa46dc40c13eab077bf05e3a333584d18d089f
Dmitriy <d.u.zheliabin@gmail.com>
2016-04-05 15:25:49 +0300

    add Search

--- app/src/main/java/com/example/dmitro/mediaplayer/ListOfSongs.java
+++ app/src/main/java/com/example/dmitro/mediaplayer/ListOfSongs.java
@@ -1,6 +1,6 @@
 package com.example.dmitro.mediaplayer;
 
-import android.app.ListActivity;
+import android.app.SearchManager;
 import android.content.Context;
 import android.database.Cursor;
 import android.media.MediaPlayer;
@@ -10,48 +10,55 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.SeekBar;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ListOfSongs extends AppCompatActivity {
 
     private static final int UPDATE_FREQUENCY = 500;
-    private static final int STEP_VALUE = 4000;
-    //адаптер
+//    private static final int STEP_VALUE = 4000;
+    //adapter
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    //контроллер
-    private TextView selelctedFile;
+    private MyRecyclerAdapter mAdapter;
+    //control
     private SeekBar seekbar;
     private MediaPlayer mediaPlayer;
     private ImageButton playButton;
     private ImageButton prevButton;
     private ImageButton nextButton;
     private ImageButton stopButton;
-    //дааные музыки
+    //data for music
     private Cursor cursor;
-    private String[] dataTitle;
-    private String[] dataArtist;
-    private String[] dataAlbum;
-    private String[] dataDuration;
+    private ArrayList<String> arrayListTitle;
+    private ArrayList<String> arrayListArtist;
+    private ArrayList<String> arrayListAlbum;
+    private ArrayList<String> arrayListDuration;
     private ImageButton temporaryMusikImage;
     private int numberPosition;
+    //TextView for player
+    private TextView titleIsPlay;
+    private TextView artistIsPlay;
+    private TextView albumIsPlay;
 
     private boolean isStarted = true;
-    private String currentFile;
     private boolean isMoveingSeekBar = false;
 
     private final Handler handler = new Handler();
@@ -66,7 +73,9 @@ public class ListOfSongs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_songs);
 
-        selelctedFile = (TextView) findViewById(R.id.selected_file);
+        titleIsPlay = (TextView) findViewById(R.id.title_is_play);
+        artistIsPlay = (TextView) findViewById(R.id.artist_is_play);
+        albumIsPlay = (TextView) findViewById(R.id.album_is_play);
         seekbar = (SeekBar) findViewById(R.id.seek_bar);
         playButton = (ImageButton) findViewById(R.id.play);
         prevButton = (ImageButton) findViewById(R.id.previous);
@@ -79,17 +88,17 @@ public class ListOfSongs extends AppCompatActivity {
         mediaPlayer.setOnErrorListener(onError);
         seekbar.setOnSeekBarChangeListener(seekBarChanged);
 
-        //массивы для данных
-        ArrayList<String> arrayListTitle = new ArrayList<>();
-        ArrayList<String> arrayListArtist = new ArrayList<>();
-        ArrayList<String> arrayListAlbum = new ArrayList<>();
-        ArrayList<String> arrayListDuration = new ArrayList<>();
-        //берем все аудио файлы в системе
+        //ArrayList for music's data
+        arrayListTitle = new ArrayList<>();
+        arrayListArtist = new ArrayList<>();
+        arrayListAlbum = new ArrayList<>();
+        arrayListDuration = new ArrayList<>();
+        //take all the audio files on the system
         cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
         if (cursor == null) {
         } else if (!cursor.moveToFirst()) {
         } else {
-            //берем параметры для адаптера
+            //take parameters for the adapter
             while (cursor.isAfterLast() == false) {
                 arrayListArtist.add(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.AudioColumns.ARTIST)));
                 arrayListAlbum.add(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.AudioColumns.ALBUM)));
@@ -103,11 +112,6 @@ public class ListOfSongs extends AppCompatActivity {
                 cursor.moveToNext();
             }
         }
-        //переделываем в простой массив для скорости и простоты работы с адаптером
-        dataAlbum = arrayListAlbum.toArray(new String[arrayListAlbum.size()]);
-        dataArtist = arrayListArtist.toArray(new String[arrayListArtist.size()]);
-        dataDuration = arrayListDuration.toArray(new String[arrayListDuration.size()]);
-        dataTitle = arrayListTitle.toArray(new String[arrayListTitle.size()]);
 
         // adapter
         mRecyclerView = (RecyclerView) findViewById(R.id.list);
@@ -118,34 +122,58 @@ public class ListOfSongs extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(dataTitle, dataArtist, dataAlbum, dataDuration, R.drawable.ic_play_arrow_black_24dp);
+        mAdapter = new MyRecyclerAdapter(arrayListTitle, arrayListArtist, arrayListAlbum,
+                arrayListDuration, R.drawable.ic_play_arrow_black_24dp);
         mRecyclerView.setAdapter(mAdapter);
-//        Cursor cursor = getContentResolver().query(
-//                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
-//
-//        if (null != cursor) {
-//            cursor.moveToFirst();
-//
-//            mediaAdapter = new MediaCursorAdapter(this, R.layout.list_of_item, cursor);
-//
-//            setListAdapter(mediaAdapter);
-//        }
-            playButton.setOnClickListener(onButtonClick);
-            nextButton.setOnClickListener(onButtonClick);
-            prevButton.setOnClickListener(onButtonClick);
-            stopButton.setOnClickListener(onButtonClick);
 
-
+        //player's buttons
+        playButton.setOnClickListener(onButtonClick);
+        nextButton.setOnClickListener(onButtonClick);
+        prevButton.setOnClickListener(onButtonClick);
+        stopButton.setOnClickListener(onButtonClick);
     }
 
-//    @Override
-//    protected void onListItemClick(ListView list, View view, int position, long id) {
-//        super.onListItemClick(list, view, position, id);
-//
-//        currentFile = (String) view.getTag();
-//
-//        startPlay(currentFile);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list_of_songs, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+
+                mAdapter.filter(query);
+
+                return true;
+
+            }
+
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onDestroy() {
@@ -159,17 +187,18 @@ public class ListOfSongs extends AppCompatActivity {
         mediaPlayer = null;
     }
 
-    private void startPlay(String file) {
-        Log.i("Selected: ", file);
-
-        selelctedFile.setText(file);
+    private void startPlay() {
+        titleIsPlay.setText(arrayListTitle.get(numberPosition));
+        artistIsPlay.setText(arrayListArtist.get(numberPosition));
+        albumIsPlay.setText(arrayListAlbum.get(numberPosition));
         seekbar.setProgress(0);
-
+        cursor.moveToPosition(numberPosition);
         mediaPlayer.stop();
         mediaPlayer.reset();
 
         try {
-            mediaPlayer.setDataSource(file);
+            mediaPlayer.setDataSource(cursor.getString(
+                    cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
             mediaPlayer.prepare();
             mediaPlayer.start();
         } catch (IllegalArgumentException e) {
@@ -206,24 +235,27 @@ public class ListOfSongs extends AppCompatActivity {
         handler.postDelayed(updatePositionRunnable, UPDATE_FREQUENCY);
     }
 
-    //Адаптер
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        String[] dataTitle;
-        String[] dataArtist;
-        String[] dataAlbum;
-        String[] dataDuration;
-        int image_for_list;
+    // The class Adapter extends RecyclerView.Adapter
+    public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+        private ArrayList<String> dataTitle;
+        private ArrayList<String> cleanCopyDataTitle;
+        private ArrayList<String> dataArtist;
+        private ArrayList<String> dataAlbum;
+        private ArrayList<String> dataDuration;
+        private int imageForList;
 
-        public MyAdapter(String[] dataTitle, String[] dataArtist, String[] dataAlbum, String[] dataDuration, int image_for_list) {
-            this.dataTitle = dataTitle;
-            this.dataArtist = dataArtist;
-            this.dataAlbum = dataAlbum;
-            this.dataDuration = dataDuration;
-            this.image_for_list = image_for_list;
+        public MyRecyclerAdapter(ArrayList<String> arrayDataTitle, ArrayList<String> arrayDataArtist,
+                                 ArrayList<String> arrayDataAlbum, ArrayList<String> arrayDataDuration, int image_for_list) {
+            dataTitle = arrayDataTitle;
+            cleanCopyDataTitle = dataTitle;
+            dataArtist = arrayDataArtist;
+            dataAlbum = arrayDataAlbum;
+            dataDuration = arrayDataDuration;
+            this.imageForList = image_for_list;
         }
 
         @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public MyRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.list_of_item, parent, false);
             ViewHolder VH = new ViewHolder(view);
@@ -232,12 +264,12 @@ public class ListOfSongs extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(final ViewHolder holder, final int position) {
-            holder.title.setText(this.dataTitle[position]);
-            holder.album.setText(this.dataAlbum[position]);
-            holder.artist.setText(this.dataArtist[position]);
-            holder.duration.setText(this.dataDuration[position]);
+            holder.title.setText(dataTitle.get(position));
+            holder.album.setText(dataAlbum.get(position));
+            holder.artist.setText(dataArtist.get(position));
+            holder.duration.setText(dataDuration.get(position));
 
-            holder.image_for_list.setImageResource(image_for_list);
+            holder.image_for_list.setImageResource(imageForList);
             holder.image_for_list.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -248,17 +280,15 @@ public class ListOfSongs extends AppCompatActivity {
                     holder.image_for_list.setImageResource(R.drawable.ic_pause_black_24dp);
                     temporaryMusikImage = holder.image_for_list;
                     numberPosition = position;
-                    cursor.moveToPosition(position);
-                    
-                    startPlay(cursor.getString(
-                            cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
+
+                    startPlay();
                 }
             });
         }
 
         @Override
         public int getItemCount() {
-            return dataTitle.length;
+            return dataTitle.size();
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
@@ -277,58 +307,21 @@ public class ListOfSongs extends AppCompatActivity {
                 image_for_list = (ImageButton) vItem.findViewById(R.id.image_for_list);
             }
         }
+        public void filter(String charText) {
+            charText = charText.toLowerCase(Locale.getDefault());
+            dataTitle = new ArrayList<String>();
+            if (charText.length() == 0) {
+                dataTitle.addAll(cleanCopyDataTitle);
+            } else {
+                for (String item : cleanCopyDataTitle) {
+                    if (item.toLowerCase(Locale.getDefault()).contains(charText)) {
+                        dataTitle.add(item);
+                    }
+                }
+            }
+            notifyDataSetChanged();
+        }
     }
-
-//    private class MediaCursorAdapter extends SimpleCursorAdapter {
-//
-//        public MediaCursorAdapter(Context context, int layout, Cursor c) {
-//            super(context, layout, c,
-//                    new String[]{MediaStore.Audio.AudioColumns.ARTIST,
-//                            MediaStore.MediaColumns.DISPLAY_NAME, MediaStore.MediaColumns.TITLE,
-//                            MediaStore.Audio.AudioColumns.DURATION},
-//                    new int[]{R.id.title, R.id.album, R.id.artist, R.id.duration}
-//            );
-//        }
-//
-//        @Override
-//        public void bindView(View view, Context context, Cursor cursor) {
-//            TextView title = (TextView) view.findViewById(R.id.title);
-//            TextView album = (TextView) view.findViewById(R.id.album);
-//            TextView artist = (TextView) view.findViewById(R.id.artist);
-//            TextView duration = (TextView) view.findViewById(R.id.duration);
-//
-//            artist.setText(cursor.getString(
-//                    cursor.getColumnIndex(MediaStore.Audio.AudioColumns.ARTIST)));
-//
-//            album.setText(cursor.getString(
-//                    cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME)));
-//
-//            title.setText(cursor.getString(
-//                    cursor.getColumnIndex(MediaStore.MediaColumns.TITLE)));
-//
-//            long durationInMs = Long.parseLong(cursor.getString(
-//                    cursor.getColumnIndex(MediaStore.Audio.AudioColumns.DURATION)));
-//
-//            double durationInMin = ((double) durationInMs / 1000.0) / 60.0;
-//
-//            durationInMin = new BigDecimal(Double.toString(durationInMin))
-//                    .setScale(2, BigDecimal.ROUND_UP).doubleValue();
-//
-//            duration.setText("" + durationInMin);
-//
-//            view.setTag(cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA)));
-//        }
-//
-//        @Override
-//        public View newView(Context context, Cursor cursor, ViewGroup parent) {
-//            LayoutInflater inflater = LayoutInflater.from(context);
-//            View v = inflater.inflate(R.layout.list_of_item, parent, false);
-//
-//            bindView(v, context, cursor);
-//
-//            return v;
-//        }
-//    }
 
     private View.OnClickListener onButtonClick = new View.OnClickListener() {
 
@@ -347,7 +340,7 @@ public class ListOfSongs extends AppCompatActivity {
 
                             updatePosition();
                         } else {
-                            startPlay(currentFile);
+                            startPlay();
                         }
                     }
 
@@ -361,27 +354,30 @@ public class ListOfSongs extends AppCompatActivity {
                     break;
                 }
                 case R.id.next: {
-
-                    int seekto = mediaPlayer.getCurrentPosition() + STEP_VALUE;
-
-                    if (seekto > mediaPlayer.getDuration())
-                        seekto = mediaPlayer.getDuration();
-
-                    mediaPlayer.pause();
-                    mediaPlayer.seekTo(seekto);
-                    mediaPlayer.start();
+                    numberPosition = numberPosition+1;
+                    startPlay();
+//                    int seekto = mediaPlayer.getCurrentPosition() + STEP_VALUE;
+//
+//                    if (seekto > mediaPlayer.getDuration())
+//                        seekto = mediaPlayer.getDuration();
+//
+//                    mediaPlayer.pause();
+//                    mediaPlayer.seekTo(seekto);
+//                    mediaPlayer.start();
 
                     break;
                 }
                 case R.id.previous: {
-                    int seekto = mediaPlayer.getCurrentPosition() - STEP_VALUE;
-
-                    if (seekto < 0)
-                        seekto = 0;
-
-                    mediaPlayer.pause();
-                    mediaPlayer.seekTo(seekto);
-                    mediaPlayer.start();
+                    numberPosition = numberPosition-1;
+                    startPlay();
+//                    int seekto = mediaPlayer.getCurrentPosition() - STEP_VALUE;
+//
+//                    if (seekto < 0)
+//                        seekto = 0;
+//
+//                    mediaPlayer.pause();
+//                    mediaPlayer.seekTo(seekto);
+//                    mediaPlayer.start();
 
                     break;
                 }
